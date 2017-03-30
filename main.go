f18f8c0315bfc319f2e2c9414bcfecdbc54e0884
Neeraj <nir.neeraj@gmail.com>
2017-03-30 15:08:44 +0800

    Added message and attachments

--- main.go
+++ main.go
@@ -51,49 +51,43 @@ type EmailAddress struct {
 	Address string `json:"address"`
 }
 
+type SenderItem struct {
+	Email EmailAddress `json:"emailAddress"`
+}
+
+type FromItem struct {
+	Email EmailAddress `json:"emailAddress"`
+}
+
+type RecipientItems struct {
+	Email EmailAddress `json:"emailAddress"`
+}
+
 type EmailBody struct {
 	ContentType string `json:"contentType"`
-	content     string `json:"content"`
+	Content     string `json:"content"`
 }
 
 type Message struct {
-	Id           string         `json:"id"`
-	CreateDate   string         `json:"createdDateTime"`
-	SentDate     string         `json:"sentDateTime"`
-	RecievedDate string         `json:"recievedDateTime"`
-	Subject      string         `json:"subject"`
-	Preview      string         `json:"bodyPreview"`
-	IsRead       bool           `json:"isRead"`
-	IsDraft      bool           `json:"isDraft"`
-	Body         EmailBody      `json:"body"`
-	Sender       EmailAddress   `json:"sender"`
-	Recipients   []EmailAddress `json:"toRecipients"`
+	Id           string    `json:"id"`
+	CreateDate   string    `json:"createdDateTime"`
+	SentDate     string    `json:"sentDateTime"`
+	RecievedDate string    `json:"receivedDateTime"`
+	Subject      string    `json:"subject"`
+	Preview      string    `json:"bodyPreview"`
+	IsRead       bool      `json:"isRead"`
+	IsDraft      bool      `json:"isDraft"`
+	Body         EmailBody `json:"body"`
+	Attachments  bool      `json:"hasAttachments"`
 }
 
 type MessageList struct {
-	Context  string    `json:"@odata.context"`
-	NextLink string    `json:"@odata.nextLink"`
-	Value    []Message `json:"value"`
-}
-
-func toGraphToken(body []byte) (*GraphToken, error) {
-
-	var token = new(GraphToken)
-	if err := json.Unmarshal(body, &token); err != nil {
-		return nil, err
-	}
-
-	return token, nil
-}
-
-func toUser(body []byte) (*User, error) {
-
-	var user = new(User)
-	if err := json.Unmarshal(body, &user); err != nil {
-		return nil, err
-	}
-
-	return user, nil
+	Context    string           `json:"@odata.context"`
+	NextLink   string           `json:"@odata.nextLink"`
+	Message    []Message        `json:"value"`
+	Sender     SenderItem       `json:"sender"`
+	From       FromItem         `json:"from"`
+	Recipients []RecipientItems `json:"toRecipients"`
 }
 
 func getAuthCode() (string, error) {
@@ -132,6 +126,16 @@ func getAuthCode() (string, error) {
 	}
 
 	return code, nil
+}
+
+func toGraphToken(body []byte) (*GraphToken, error) {
+
+	var token = new(GraphToken)
+	if err := json.Unmarshal(body, &token); err != nil {
+		return nil, err
+	}
+
+	return token, nil
 }
 
 func getAccessToken(authCode string) (*GraphToken, error) {
@@ -184,6 +188,16 @@ func printAccesstoken(token *GraphToken) {
 	fmt.Println("*****************************")
 }
 
+func toUser(body []byte) (*User, error) {
+
+	var user = new(User)
+	if err := json.Unmarshal(body, &user); err != nil {
+		return nil, err
+	}
+
+	return user, nil
+}
+
 func getUserInfo(token string) (*User, error) {
 
 	graphUrl := GRAPH_HOST + GRAPH_VERSION + "me"
@@ -228,6 +242,124 @@ func printUserInfo(user *User) {
 	fmt.Println("*****************************")
 }
 
+func toMessageList(body []byte) (*MessageList, error) {
+
+	var msgList = new(MessageList)
+	if err := json.Unmarshal(body, &msgList); err != nil {
+		return nil, err
+	}
+
+	return msgList, nil
+}
+
+func getMessageList(token string) (*MessageList, error) {
+
+	graphUrl := GRAPH_HOST + GRAPH_VERSION + "me/messages"
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", graphUrl, nil)
+
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New(resp.Status)
+	}
+
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var msgList *MessageList
+	msgList, err = toMessageList(bodyBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return msgList, nil
+}
+
+func printMessageList(msgList *MessageList) {
+	fmt.Println("Message Id list********************")
+	for _, msg := range msgList.Message {
+		fmt.Println(msg.Id)
+	}
+
+	fmt.Println("Next link: ", msgList.NextLink)
+}
+
+func toMessage(body []byte) (*Message, error) {
+
+	var msg = new(Message)
+	if err := json.Unmarshal(body, &msg); err != nil {
+		return nil, err
+	}
+
+	return msg, nil
+}
+
+func getMessage(msgid, token string) (*Message, error) {
+
+	graphUrl := GRAPH_HOST + GRAPH_VERSION + "me/messages/" + msgid
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("GET", graphUrl, nil)
+
+	req.Header.Add("Authorization", "Bearer "+token)
+
+	resp, err := client.Do(req)
+
+	if err != nil {
+		return nil, err
+	}
+
+	if resp.StatusCode != 200 {
+		return nil, errors.New(resp.Status)
+	}
+
+	defer resp.Body.Close()
+
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	var msg *Message
+	msg, err = toMessage(bodyBytes)
+	if err != nil {
+		return nil, err
+	}
+
+	return msg, nil
+}
+
+func printMessage(msgList *MessageList, msg *Message) {
+	fmt.Println("Message********************")
+	fmt.Println("From:", msgList.Sender.Email.Name, "(", msgList.Sender.Email.Address, ")")
+
+	fmt.Println("Recipients:")
+	for _, rec := range msgList.Recipients {
+		fmt.Println(rec.Email.Name, "(", rec.Email.Address, ")")
+	}
+
+	fmt.Println("Created ", msg.CreateDate)
+	fmt.Println("Recieved ", msg.RecievedDate)
+	fmt.Println("Subject:", msg.Subject)
+	fmt.Println("Preview:", msg.Preview)
+	fmt.Println("Body:")
+	fmt.Println(msg.Body.Content)
+	fmt.Println()
+	fmt.Println("Attachments ", msg.Attachments)
+}
+
 func main() {
 
 	authCode, err := getAuthCode()
@@ -249,8 +381,32 @@ func main() {
 		log.Println("Trouble getting User info", err)
 		return
 	}
-
 	printUserInfo(user)
+	fmt.Println()
+
+	msgList, err := getMessageList(token.AccessToken)
+	if err != nil {
+		log.Println("Trouble getting message list", err)
+		return
+	}
+	printMessageList(msgList)
+	fmt.Println()
+
+	fmt.Println("Enter msg id")
+	var msgId string
+	_, err = fmt.Scan(&msgId)
+
+	if err != nil {
+		log.Fatal("Error reading msgId from console")
+		return
+	}
+
+	msg, err := getMessage(msgId, token.AccessToken)
+	if err != nil {
+		log.Println("Trouble getting message", err)
+		return
+	}
+	printMessage(msgList, msg)
 	fmt.Println()
 
 }
